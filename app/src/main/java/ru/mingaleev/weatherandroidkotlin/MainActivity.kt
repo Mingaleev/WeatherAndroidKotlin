4bae17461e7962186c2aabbc69ab1e3cbbd1c5b8
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-26 20:47:21 +0300

    Урок 2 | Связал LD c фрагментом

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/MainActivity.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.mingaleev.weatherandroidkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.mingaleev.weatherandroidkotlin.databinding.ActivityMainBinding
+import ru.mingaleev.weatherandroidkotlin.view.weatherlist.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState==null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container, WeatherListFragment.newInstance()).commit()
+        }
     }
 }
--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListViewModel.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListViewModel.kt
@@ -2,7 +2,16 @@ package ru.mingaleev.weatherandroidkotlin.view.weatherlist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.mingaleev.weatherandroidkotlin.viewmodel.AppState
+import java.lang.Thread.sleep
 
-class WeatherListViewModel(val liveData:MutableLiveData<Any> = MutableLiveData<Any>()): ViewModel() {
+class WeatherListViewModel(val liveData:MutableLiveData<AppState> = MutableLiveData<AppState>()): ViewModel() {
 
+    fun sentRequest() {
+        liveData.value =AppState.Loading
+        Thread{
+            sleep(2000L)
+            liveData.value =AppState.Success(Any())
+        }
+    }
 }
