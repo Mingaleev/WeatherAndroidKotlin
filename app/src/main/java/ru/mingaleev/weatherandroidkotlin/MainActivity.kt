790e9a990d2893900615ada7b7f3d639a3cf1413
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-26 12:30:42 +0300

    Урок 2 | Добавил ViewBinding в MainActivity

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/MainActivity.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/MainActivity.kt
@@ -2,11 +2,14 @@ package ru.mingaleev.weatherandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.mingaleev.weatherandroidkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
 }
