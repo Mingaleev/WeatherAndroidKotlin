61e224c3c941f298a9f191abcc2300e07ce6a395
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-26 11:21:29 +0300

    Initial commit

---
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.mingaleev.weatherandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
