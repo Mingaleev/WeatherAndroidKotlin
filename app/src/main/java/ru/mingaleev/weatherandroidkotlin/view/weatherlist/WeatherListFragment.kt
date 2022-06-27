566e7c02884dc78c100744c1cf81b26adbda7b55
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-27 21:09:33 +0300

    Урок 3 | Добавил обработку переключения списков локаций

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import ru.mingaleev.weatherandroidkotlin.R
 import ru.mingaleev.weatherandroidkotlin.databinding.FragmentWeatherListBinding
 import ru.mingaleev.weatherandroidkotlin.viewmodel.AppState
 
@@ -16,6 +17,8 @@ class WeatherListFragment : Fragment() {
     companion object {
         fun newInstance() = WeatherListFragment()
     }
+
+    var fabRForWorld = false
 
     private lateinit var binding: FragmentWeatherListBinding
     lateinit var viewModel: WeatherListViewModel
@@ -31,12 +34,21 @@ class WeatherListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(WeatherListViewModel::class.java)
-        viewModel.getLiveData().observe(viewLifecycleOwner,object : Observer<AppState>{
+        viewModel.getLiveData().observe(viewLifecycleOwner, object : Observer<AppState> {
             override fun onChanged(t: AppState) {
                 renderData(t)
             }
         })
-        viewModel.sentRequest()
+
+        binding.FragmentFAB.setOnClickListener() {
+            if (fabRForWorld) {
+                viewModel.getWeatherListForRussia()
+                binding.FragmentFAB.setImageResource(R.drawable.ic_russia)
+            } else {
+                viewModel.getWeatherListForWorld()
+                binding.FragmentFAB.setImageResource(R.drawable.ic_earth)
+            }
+        }
     }
 
     fun renderData(appState: AppState){
@@ -48,7 +60,7 @@ class WeatherListFragment : Fragment() {
             AppState.Loading -> {
                 binding.loadingLayout.visibility = View.VISIBLE
             }
-            is AppState.Success -> {
+            is AppState.SuccessListCity -> {
                 val result = appState.weatherData
                 binding.loadingLayout.visibility = View.GONE
                 binding.cityName.text = result.city.name
@@ -56,6 +68,7 @@ class WeatherListFragment : Fragment() {
                 binding.feelsLikeValue.text = result.feelsLike.toString()
                 binding.cityCoordinates.text = "${result.city.lat} / ${result.city.lon}"
             }
+            is AppState.SuccessCity -> TODO()
         }
     }
 }
