24ae117630325a1b0e4d43bc5d258111ad56c973
arturmingaleev <art.mingaleev@yandex.ru>
2022-07-04 19:45:51 +0300

    Урок 4 | Реализовал Snackbar

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/view/weatherlist/WeatherListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
@@ -57,8 +56,13 @@ class WeatherListFragment : Fragment(), OnItemClick {
     private fun renderData(appState: AppState){
         when (appState) {
             is AppState.Error -> {
-                Toast.makeText(requireContext(), appState.error.toString(), Toast.LENGTH_SHORT)
-                    .show()
+                binding.mainFragmentRecyclerView.createAndShow("Ошибка загрузки", "Еще раз", Snackbar.LENGTH_LONG) { _ ->
+                    if (!fabRForWorld) {
+                        viewModel.getWeatherListForRussia()
+                    } else {
+                        viewModel.getWeatherListForWorld()
+                    }
+                }
             }
             AppState.Loading -> {
                 binding.setStateFragment(appState)
@@ -82,6 +86,9 @@ class WeatherListFragment : Fragment(), OnItemClick {
             }
         }
     }
+
+    fun View.createAndShow(text: String, actionText: String, duration: Int, action: (v: View) -> Unit) {
+        Snackbar.make(this, text, duration).setAction(actionText, action).show() }
 
     override fun onItemClick(weather: Weather) {
         requireActivity().supportFragmentManager.beginTransaction().hide(this).add(
