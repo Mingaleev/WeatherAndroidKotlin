da8ec2a994ee7a05ae2c695d32a6b7cb997bb26b
arturmingaleev <art.mingaleev@yandex.ru>
2022-07-05 18:05:35 +0300

    Урок 5 | Добавил классы DTO через плагин JSON to Kotlin

---
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/model/dto/WeatherDTO.kt
@@ -0,0 +1,16 @@
+package ru.mingaleev.weatherandroidkotlin.model.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherDTO(
+    val fact: Fact,
+    @SerializedName("forecast")
+    val forecast: Forecast,
+    @SerializedName("info")
+    val info: Info,
+    @SerializedName("now")
+    val now: Int,
+    @SerializedName("now_dt")
+    val nowDt: String
+)
