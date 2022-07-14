7f1b93b0417c1bfe222e8decacf750b3ebaf4e36
arturmingaleev <art.mingaleev@yandex.ru>
2022-07-14 10:04:39 +0300

    Урок 5 работа над ошибками | Проработал исключения в запросе

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/utils/WeatherLoader.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/utils/WeatherLoader.kt
@@ -8,8 +8,10 @@ import ru.mingaleev.weatherandroidkotlin.view.details.OnResponse
 import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 object WeatherLoader {
 
@@ -17,17 +19,20 @@ object WeatherLoader {
 
 
         val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
-        var myConnection: HttpURLConnection? = null
-            myConnection = uri.openConnection() as HttpURLConnection
-            myConnection.readTimeout = 5000
-            myConnection.addRequestProperty(API_KEY_YANDEX, BuildConfig.WEATHER_API_KEY)
+
         Thread {
+            var myConnection: HttpsURLConnection? = null
             try {
+                myConnection = uri.openConnection() as HttpsURLConnection
+                myConnection.readTimeout = 5000
+                myConnection.addRequestProperty(API_KEY_YANDEX, BuildConfig.WEATHER_API_KEY)
                 val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
                 val weatherDTO = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
                 onResponse.onResponse(weatherDTO)
-            } catch (e: FileNotFoundException) {
-                Log.e("!!!", "Ошибка загрузки", e)
+            } catch (e: Exception) {
+                myConnection?.disconnect()
+            } finally {
+                myConnection?.disconnect()
             }
         }.start()
     }
