9fb60926664261ac5bdcd48c9b8e88b123e8f33e
arturmingaleev <art.mingaleev@yandex.ru>
2022-07-24 15:29:29 +0300

    Урок 7 работа над ошибками | Перенес создание ретрофит клиента в MyApp для экономии ресурсов

--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/MyApp.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/MyApp.kt
@@ -2,6 +2,9 @@ package ru.mingaleev.weatherandroidkotlin
 
 import android.app.Application
 import androidx.room.Room
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.mingaleev.weatherandroidkotlin.model.room.WeatherDatabase
 import ru.mingaleev.weatherandroidkotlin.utils.ROOM_DB_NAME
 
@@ -14,7 +17,10 @@ class MyApp: Application() {
     companion object {
         private var myApp: MyApp? = null
         private var weatherDatabase: WeatherDatabase? = null
+        private var retrofit: Retrofit.Builder? = null
+
         fun getMyApp() = myApp!!
+
         fun getWeatherDatabase(): WeatherDatabase {
             if (weatherDatabase == null) {
                 weatherDatabase =
@@ -22,6 +28,17 @@ class MyApp: Application() {
                         .build()
             }
             return weatherDatabase!!
+        }
+
+        fun getRetrofitYandex(): Retrofit.Builder? {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.weather.yandex.ru")
+                    .addConverterFactory(
+                        GsonConverterFactory.create(GsonBuilder().setLenient().create())
+                    )
+            }
+            return retrofit
         }
     }
 }
--- app/src/main/java/ru/mingaleev/weatherandroidkotlin/model/retrofit/RepositoryWeatherByLocationRetrofitImpl.kt
+++ app/src/main/java/ru/mingaleev/weatherandroidkotlin/model/retrofit/RepositoryWeatherByLocationRetrofitImpl.kt
@@ -1,13 +1,11 @@
 package ru.mingaleev.weatherandroidkotlin.model.retrofit
 
-import com.google.gson.GsonBuilder
 import okio.IOException
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.mingaleev.weatherandroidkotlin.BuildConfig
+import ru.mingaleev.weatherandroidkotlin.MyApp.Companion.getRetrofitYandex
 import ru.mingaleev.weatherandroidkotlin.domain.City
 import ru.mingaleev.weatherandroidkotlin.model.MySuperCallbackCity
 import ru.mingaleev.weatherandroidkotlin.model.RepositoryWeatherByLocation
@@ -16,29 +14,30 @@ import ru.mingaleev.weatherandroidkotlin.utils.converterWeatherDtoToWeather
 
 class RepositoryWeatherByLocationRetrofitImpl : RepositoryWeatherByLocation {
     override fun getWeather(city: City, callback: MySuperCallbackCity) {
-        val retrofitImpl = Retrofit.Builder()
-        retrofitImpl.baseUrl("https://api.weather.yandex.ru")
-        retrofitImpl.addConverterFactory(
-            GsonConverterFactory.create(
-                GsonBuilder().setLenient().create()
-            )
-        )
-        val api = retrofitImpl.build().create(WeatherAPI::class.java)
-
-        api.getWeather(BuildConfig.WEATHER_API_KEY, city.lat, city.lon)
-            .enqueue(object : Callback<WeatherDTO> {
-                override fun onResponse(call: Call<WeatherDTO>, response: Response<WeatherDTO>) {
-                    if (response.isSuccessful && response.body() != null) {
-                        callback.onResponse(converterWeatherDtoToWeather(response.body()!!, city.name))
-                    } else {
-                        callback.onFailure(IOException("403/404 Error"))
+        getRetrofitYandex()?.run {
+            build().create(WeatherAPI::class.java)
+                .getWeather(BuildConfig.WEATHER_API_KEY, city.lat, city.lon)
+                .enqueue(object : Callback<WeatherDTO> {
+                    override fun onResponse(
+                        call: Call<WeatherDTO>,
+                        response: Response<WeatherDTO>
+                    ) {
+                        if (response.isSuccessful && response.body() != null) {
+                            callback.onResponse(
+                                converterWeatherDtoToWeather(
+                                    response.body()!!,
+                                    city.name
+                                )
+                            )
+                        } else {
+                            callback.onFailure(IOException("403/404 Error"))
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<WeatherDTO>, t: Throwable) {
-                    callback.onFailure(t as IOException /* = java.io.IOException */) //костыль
-                }
-            })
-
+                    override fun onFailure(call: Call<WeatherDTO>, t: Throwable) {
+                        callback.onFailure(t as IOException /* = java.io.IOException */) //костыль
+                    }
+                })
+        }
     }
 }
